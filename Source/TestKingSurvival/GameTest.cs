4aae6d52f03ce324a9abd5ebca2400974e900a07
Diana Konqrska <diana.konqrska@gmail.com>
2013-05-11 16:31:38 +0300

    TestKingSurvival project added with class GameTest

---
+++ Source/TestKingSurvival/GameTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using KingSurvival;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestKingSurvival
+{
+    [TestClass]
+    public class GameTest
+    {
+    }
+}
